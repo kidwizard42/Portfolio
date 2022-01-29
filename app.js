058a02daee6b5a57afb8139bd5e767950f63a357
Darwood Antoine <darwoodantoine@Darwoods-MacBook-Pro.local>
2022-01-29 17:25:09 -0500

    carousel working needs mobile design

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 $(() => {
-    $menuDrop = $("#main-nav-ul-hamburger")
-    $menuDrop.on("click",() => {
-        $(".main-nav-li").toggleClass( "display-none" )
-        console.log("works")
-    })
+  $menuDrop = $("#main-nav-ul-hamburger")
+  $menuDrop.on("click", () => {
+    $(".main-nav-li").toggleClass("display-none")
+    console.log("works")
+  })
+
+  let projectIndex = 0
+  $("#right").on("click", () => {
+    $("#section-wrapper").children().eq(projectIndex).css("display", "none")
+    projectIndex++
+    // need to change this when I add more projects
+    if (projectIndex > 2) {
+      projectIndex = 0
+    }
+    $("#section-wrapper").children().eq(projectIndex).css("display", "block")
+  })
+
+  $("#left").on("click", () => {
+    $("#section-wrapper").children().eq(projectIndex).css("display", "none")
+    projectIndex--
+    if (projectIndex < 0) {
+      //   need to change this when I add more projects
+      projectIndex = 2
+    }
+    $("#section-wrapper").children().eq(projectIndex).css("display", "block")
+  })
 })
